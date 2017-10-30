1e50028eaa68c3cfe5d7730bdf03ab7f054eeea9
AndreaNim <32754341+AndreaNim@users.noreply.github.com>
2017-10-30 21:52:30 +0530

    created square area class

---
+++ Java-square-area-calculator/Square.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Square {
+    public static void main (String [] args){
+        Scanner in = new Scanner(System.in);
+        System.out.println("enter the length");
+        double length = in.nextDouble();
+        System.out.println("square are  :"+(length*length);
+
+    }
+
+}
