75077894e199b10925f8778ce90ac1ab058f6007
Ujjwal3009 <50870185+Ujjwal3009@users.noreply.github.com>
2020-09-14 21:52:05 +0530

    Created methid for adding two number .
    By creating method code is reusable in any class with single line.

--- Java-calculator/Sum.java
+++ Java-calculator/Sum.java
@@ -1,14 +1,20 @@
 import java.util.Scanner;
 public class Sum
 {
-	public static void main(String[]args)
-	{
-        	Scanner sc=new Scanner(System.in);
+static void add()	
+   {
+	Scanner sc=new Scanner(System.in);
         	System.out.println("enter first number: ");
         	int num1=sc.nextInt();
 		System.out.println("enter second number: ");
         	int num2=sc.nextInt();
 		int Total=num1+num2;
 		System.out.println("the sum of "+num1+" and "+num2+" is "+Total);
+	
+    }
+	public static void main(String[]args)
+	{
+             Sum.add();
+        	
 	}
 }
