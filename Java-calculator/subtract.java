ca6e626575ca909bfad2e90c556fe9315b52bd76
imsilent681028 <110386374+imsilent681028@users.noreply.github.com>
2022-08-01 15:16:55 +0530

    Create subtract.java

---
+++ Java-calculator/subtract.java
@@ -0,0 +1,9 @@
+public static void main(string args[]){
+  scanner s=new scanner(System.in);
+  System.out.print("Enter two numbers: ");
+  int a,b;
+  a=s.nextInt;
+  b=s.nextInt;
+  int sub=a-b;
+  System.out.println("The subtraction is: ",+sub);
+}
